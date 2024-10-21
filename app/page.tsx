fe9af8a46c35aa81600275822232c82676ed3a03
Daria K <kurilenko.dariia@gmail.com>
2024-10-21 21:18:39 +0300

    added shadcn components, created layout, connected lucide icons

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+    <h1>Home</h1>
+    <Button variant="outline">корзина</Button>
+    </>
+    
+  );
+}
